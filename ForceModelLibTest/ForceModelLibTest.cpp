fad128d815c1608d9c63dee6446edfeae980c67b
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2016-10-21 18:26:13 -0200

    ForceModelLibTest updated. Runnig with no error.

--- ForceModelLibTest/ForceModelLibTest.cpp
+++ ForceModelLibTest/ForceModelLibTest.cpp
@@ -3,15 +3,18 @@
 int main(int argc, char **argv){
 
 	{// taylorPredictor test
+		int predictionOrder = 3;
 		vector<Vector3D> currentVector;
-			currentVector.resize(2);
+			currentVector.resize(predictionOrder);
 			currentVector[0].x() = 0.0;
 			currentVector[0].y() = 0.0;
 			currentVector[0].z() = 0.0;
 			currentVector[1].x() = 0.0;
 			currentVector[1].y() = 0.0;
 			currentVector[1].z() = 0.0;
-		int predictionOrder = 1;
+			currentVector[2].x() = 0.0;
+			currentVector[2].y() = 0.0;
+			currentVector[2].z() = 0.0;
 		double dt = 2;
 		vector<Vector3D> vectorTaylorPredictor = ForceModel::taylorPredictor(currentVector, predictionOrder, dt );
 	}
@@ -23,20 +26,23 @@ int main(int argc, char **argv){
 	}
 
 	{// gearCorrector test
+		int predictionOrder = 3;
 		vector<Vector3D> predictedVector;
-			predictedVector.resize(2);
+			predictedVector.resize(predictionOrder);
 			predictedVector[0].x() = 0.0;
 			predictedVector[0].y() = 0.0;
 			predictedVector[0].z() = 0.0;
 			predictedVector[1].x() = 0.0;
 			predictedVector[1].y() = 0.0;
 			predictedVector[1].z() = 0.0;
+			predictedVector[2].x() = 0.0;
+			predictedVector[2].y() = 0.0;
+			predictedVector[2].z() = 0.0;
 		Vector3D doubleDerivative(0., 0., 0.);
-		int predictionOrder = 1;
 		double dt = 2;
 	
 		vector<Vector3D> vectorGearCorrector = ForceModel::gearCorrector(predictedVector, doubleDerivative, predictionOrder, dt);
 	}
 
-	return 0;	
+	return 0;
 }
