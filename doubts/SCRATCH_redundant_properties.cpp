f6e64bc142a2ac8b3cba3e17bd7abe7e0b6a436e
Ruan Comelli <ruancomelli@sinmec.ufsc.br>
2017-03-09 09:52:50 -0300

    Scratch updated

--- doubts/SCRATCH_redundant_properties.cpp
+++ doubts/SCRATCH_redundant_properties.cpp
@@ -43,6 +43,13 @@ PhysicalEntity::checkPropertyDependencies()
 	this->propertyArray.checkDependencies();
 }
 
+// Ideia para o Property<T>::dependsOn( Property<Args>..., T (*f)(Args...) ):
+// Ela pode converter f para uma função g( Any&, Any... ), em que o primeiro Any corresponde
+// ao T. essa função g é:
+// void g(Any& x, Any... values){
+// 		x = f( values... )
+// }
+
 // ==============================================================================================================================
 // ==============================================================================================================================
 // PropertyContainer.cpp
@@ -66,4 +73,33 @@ PropertyContainer::checkDependencies()
 			}
 		}
 	}
+}
+
+// ==============================================================================================================================
+// ==============================================================================================================================
+// UtilsLib/functionMasker.cpp
+// ==============================================================================================================================
+
+// See http://stackoverflow.com/questions/7858817/unpacking-a-tuple-to-call-a-matching-function-pointer
+// See http://www.cplusplus.com/reference/tuple/tie/
+// See http://en.cppreference.com/w/cpp/utility/functional/function
+// See https://www.google.com.br/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=function+with+lambda+as+parameter+c%2B%2B&* for creating properties.
+//	The user may want to pass lambda expressions to constructors and so on instead of passing function pointers. See std::function.
+// See http://en.cppreference.com/w/cpp/concept/Container . Maybe we should update some of our classes to match some standard concepts.
+// See http://en.cppreference.com/w/cpp/container/array
+// See http://en.cppreference.com/w/cpp/utility/tuple/tuple_size
+// For variadic templates, see http://eli.thegreenplace.net/2014/variadic-templates-in-c/ , http://en.cppreference.com/w/cpp/language/parameter_pack and http://www.cplusplus.com/articles/EhvU7k9E/
+// For something about recursion, see http://www.informit.com/articles/article.aspx?p=30667&seqNum=4
+// For initialization, see http://en.cppreference.com/w/cpp/language/aggregate_initialization , http://en.cppreference.com/w/cpp/language/initializer_list and http://en.cppreference.com/w/cpp/language/list_initialization
+
+template<typename T, typename ... Args>
+void call( std::function<T (Args...)> f, Any& x, Many )
+{
+
+}
+
+template<typename T, typename ... Args>
+auto masker( std::function<T (Args...)> f ) 
+{
+	return 
 }
