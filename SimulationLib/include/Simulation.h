d801750a3673684df69e664b472de41b89da870d
Ruan Comelli <ruancomelli@sinmec.ufsc.br>
2017-03-13 09:30:49 -0300

    SimulationLib continued (not finished)

--- SimulationLib/include/Simulation.h
+++ SimulationLib/include/Simulation.h
@@ -1,19 +1,48 @@
 #ifndef SIMULATION_H
 #define SIMULATION_H
 
-#include <vector>
-#include <algorithm>
-#include "Particle.h"
-#include "ForceModel.h"
+// ForceModelLib
+#include <ForceModel.h>
 
-using namespace std;
+// IOLib
+#include <SphericalParticlePtrArrayKit.h>
 
+// UtilsLib
+#include <Vector3D.h>
+
+// Standard
+#include <string>
+
+using std::string;
 
 class Simulation
 {
-};
+	public:
+
+		void inputMainData(void);
+
+	private:
+		// Simulation Folders and Paths
+		string inputFolder;	// This should be something like project_root_path + "_input/"
+		string simulationName;	// This should be something like "Simulation1", or "MySimulation"
+
+		// Simulation data
+		double initialTime;
+		double timeStep;
+		double finalTime;
+		int taylorOrder;
+		int dimension;
+		int numberOfParticles;
+		int timeStepsForOutput;
+		Vector3D gravity;
+
+		// Simulation objects
+		SphericalParticlePtrArrayKit particleArray;
+		ForceModel forceModel;
+
+}; // class Simulation
 
 
-#endif
+#endif	// SIMULATION_H
 
 
--- SimulationLib/source/Simulation.cpp
+++ SimulationLib/source/Simulation.cpp
@@ -1,1 +1,33 @@
-#include "Simulation.h"
+#include <Simulation.h>
+
+// IOLib
+#include <FileReader.h>
+
+void Simulation::inputMainData(void)
+{
+	FileReader inputData(this->inputFolder + this->simulationName + "/input.txt");
+
+	// Read simulation data
+	inputData.readValue("<initialTime>", this->initialTime);
+	inputData.readValue("<timeStep>", this->timeStep);
+	inputData.readValue("<finalTime>", this->finalTime);
+	inputData.readValue("<taylorOrder>", this->taylorOrder);
+	inputData.readValue("<dimension>", this->dimension);
+	inputData.readValue("<numberOfParticles>", this->numberOfParticles);
+	inputData.readValue("<gravity>", this->gravity);
+	inputData.readValue("<timeStepsForOutput>", this->timeStepsForOutput);
+
+	// Read ForceModel
+	inputData.readValue("<ForceModelName>", forceModelName);
+
+	ForceModel forceModel;
+
+	for (auto& fm : forceModelSet)
+	{
+		if (fm.getName() == forceModelName)
+		{
+			forceModel = fm;
+			break;
+		}
+	}
+}
