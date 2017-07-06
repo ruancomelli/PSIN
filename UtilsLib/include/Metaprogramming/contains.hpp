3c60d59f5215d4ce01b68c83990f8a55e91acfec
Ruan Comelli <ruancomelli@gmail.com>
2017-07-06 15:45:07 -0300

    New metaporgramming feature (not finished)

---
+++ UtilsLib/include/Metaprogramming/contains.hpp
@@ -0,0 +1,11 @@
+#ifndef CONTAINS_HPP
+#define CONTAINS_HPP
+
+#include <type_traits>
+
+namespace traits{
+	template<typename Container, typename Element, typename ... Elements>
+	struct contains;
+}
+
+#endif
--- UtilsLib/include/Metaprogramming/type_list.hpp
+++ UtilsLib/include/Metaprogramming/type_list.hpp
@@ -1,26 +1,41 @@
-#ifndef UTILS_TYPE_LIST_HPP
-#define UTILS_TYPE_LIST_HPP
+#ifndef TYPE_LIST_HPP
+#define TYPE_LIST_HPP
 
 template<typename...Ts>
 struct type_list;
 
-template<typename T, typename U>
-struct prepend_type_to_type_list;
-
-template<typename T, typename...Ts>
-struct prepend_type_to_type_list<T, type_list<Ts...>>
+namespace traits
 {
-	using value = type_list<T, Ts...>;
+	template<typename U, typename ... Us>
+	struct contains<type_list<>, U, Us...>
+	{
+		constexpr bool value = false;
+	}
+	
+	template<typename U, typename ... Us>
+	struct contains<type_list<>, U, Us...>
+	{
+		constexpr bool value = false;
+	}
 };
 
-template<typename T, typename U>
-struct append_type_to_type_list;
-
-template<typename T, typename...Ts>
-struct append_type_to_type_list<T, type_list<Ts...>>
+template<typename...Ts>
+struct type_list
 {
-	using value = type_list<Ts..., T>;
-};
+	template<typename T>
+	using append = type_list<Ts..., T>;
 
+	template<typename T>
+	using prepend = type_list<T, Ts...>;
+
+	template<typename U, typename ... Us>
+	constexpr bool contains = traits::contains<type_list<Ts...>, U, Us...>::value;
+};
 
 #endif
+
+// WARNING: The use of this is, e.g., 'static_assert( std::is_same< type_list<int>::append<double> , type_list<int, double> >::value );'
+// That is, 'type_list<Ts...>::append<T>' is the same as 'type_list<Ts..., T>'.
+// The problem is that, in 'std::' syntax, there is a 'type', as in
+// 		std::conditional<true, int, double>::type;
+// Should we change our code? If so, this would change a lot the 'type_list', and the implementation would be more polluted.
