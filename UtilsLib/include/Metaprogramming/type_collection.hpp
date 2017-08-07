0cea96aa3c85effd41b3b884b82a074ed721da62
Ruan Comelli <ruancomelli@gmail.com>
2017-08-07 14:14:18 -0300

    I forgot to commit type_collection.hpp

---
+++ UtilsLib/include/Metaprogramming/type_collection.hpp
@@ -0,0 +1,33 @@
+#ifndef TYPE_COLLECTION_HPP
+#define TYPE_COLLECTION_HPP
+
+// Utils / Metaprogramming
+#include <Metaprogramming/traits/type_collection.hpp>
+#include <Metaprogramming/type_list.hpp>
+#include <Metaprogramming/make_unique_type_list.hpp>
+
+template<typename...Ts>
+struct type_collection
+{
+	using identity = type_collection<Ts...>;
+
+	using equivalent_type_list = make_unique_type_list<Ts...>;
+
+	template<typename U, typename ... Us>
+	constexpr static bool contains = equivalent_type_list::template contains<U, Us...>;
+
+	constexpr static bool is_empty = equivalent_type_list::is_empty;
+
+	constexpr static unsigned size = equivalent_type_list::size;
+
+	template<typename U>
+	constexpr static bool is_superset_of = traits::is_superset_of<identity, U>::value;
+
+	template<typename U>
+	constexpr static bool is_subset_of = traits::is_subset_of<identity, U>::value;
+
+	template<typename U>
+	constexpr static bool is_equal_to = is_superset_of<U> && is_subset_of<U>;
+};
+
+#endif
