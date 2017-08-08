c75a0ae32ba983ccf71e7c3521b3211ccf599a8d
Ruan Comelli <ruancomelli@gmail.com>
2017-08-08 09:55:13 -0300

    ElectrostaticForce corrected (but not tested)

--- InteractionLib/include/InteractionDefinitions/ElectrostaticForce.hpp
+++ InteractionLib/include/InteractionDefinitions/ElectrostaticForce.hpp
@@ -1,28 +1,32 @@
 #ifndef ELECTROSTATIC_FORCE_HPP
 #define ELECTROSTATIC_FORCE_HPP
 
+// PropertyLib
+#include <PropertyDefinitions.hpp>
+
 // ------------------ FORCE CALCULATION ------------------
 //		particle is the reference
 //		normalForce is the normal force applied BY neighbor TO particle
 //		tangentialForce is the tangential force applied BY neighbor TO particle
-
 struct ElectrostaticForce
 {
-	template<typename P1, typename P2>
-	static void calculate(P1 particle1, P2 particle2)
-	{
-		double k = 9 * pow(10, 9);
+	using ElectricCharge = PropertyDefinitions::ElectricCharge;
 
-		double charge1 = particle->get(electric_charge);
-		double charge2 = neighbor->get(electric_charge);
-		double r = particle->distance(neighbor);
+	template<typename P1, typename P2>
+	static void calculate(P1 & particle, P2 & neighbor)
+	{
+		double k = 9e+9;
+
+		double charge1 = particle.template get<ElectricCharge>();
+		double charge2 = neighbor.template get<ElectricCharge>();
+		double r = particle.distance(neighbor);
 
 		double force = - k * charge1 * charge2 / ( r * r );
 
-		Vector3D electricForce = force * particle->normalDirection(neighbor);
+		Vector3D electricForce = force * particle.normalDirection(neighbor);
 
-		particle->addBodyForce( electricForce );
-		neighbor->addBodyForce( - electricForce );
+		particle.addBodyForce( electricForce );
+		neighbor.addBodyForce( - electricForce );
 	}
 };
 
