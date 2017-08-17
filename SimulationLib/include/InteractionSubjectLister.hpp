653c895a72dd726eb2fb011479e70fdd5cdcd358
Ruan Comelli <ruancomelli@gmail.com>
2017-08-17 15:50:06 -0300

    InteractionSubjectLister now uses mp::combinatory

--- SimulationLib/include/InteractionSubjectLister.hpp
+++ SimulationLib/include/InteractionSubjectLister.hpp
@@ -1,19 +1,15 @@
 #ifndef INTERACTION_SUBJECT_LISTER_HPP
 #define INTERACTION_SUBJECT_LISTER_HPP
 
-// SimulationLib
-#include <MP/InteractionSubjectLister.hpp>
-
 // UtilsLib
 #include <metaprogramming.hpp>
 
 class InteractionSubjectLister
 {
 	template<typename Interactions, typename Subjects>
-	using generate_pairs = typename mp::generate_pairs<Interactions, Subjects>::type;
+	using generate_combinations = typename mp::generate_combinations<Interactions, Subjects>::type;
 
-	template<typename Interactions, typename Subjects>
-	using list = typename mp::generate_pairs<Interactions, Subjects>::type;
+	template<typename>
 };
 
 #endif
