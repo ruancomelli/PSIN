a14100ed32f7e61d9cc6de0946a68d508c02832a
Ruan Comelli <ruancomelli@gmail.com>
2017-08-17 15:49:38 -0300

    metaprogramming.hpp updated

--- UtilsLib/include/metaprogramming.hpp
+++ UtilsLib/include/metaprogramming.hpp
@@ -2,7 +2,13 @@
 #define METAPROGRAMMING_HPP
 
 #include <MP/bool_type.hpp>
+#include <MP/combinatory.hpp>
+#include <MP/concatenate.hpp>
+#include <MP/get.hpp>
+#include <MP/length.hpp>
 #include <MP/make_unique_type_list.hpp>
+#include <MP/metafunction.hpp>
+#include <MP/purge.hpp>
 #include <MP/sfinae_true.hpp>
 #include <MP/type_collection.hpp>
 #include <MP/type_list.hpp>
