9ad708679943e8c628a299e78dbe14222408e2d8
Ruan Comelli <ruancomelli@gmail.com>
2017-09-04 11:13:42 -0300

    JSONLib successfully tested

--- JSONLibTest/JSONLibTest.cpp
+++ JSONLibTest/JSONLibTest.cpp
@@ -1,5 +1,9 @@
 #define BOOST_TEST_MODULE JSONLibTest
 
+#include <fstream>
+#include <iostream>
+#include <string>
+
 // JSONLib
 #include <json.hpp>
 
@@ -7,10 +11,13 @@
 #include <Test.hpp>
 
 using namespace psin;
+using std::ifstream;
+using std::ofstream;
+using std::string;
 
-TestCase(json_Test)
+TestCase(basic_json_Test)
 {
-	using namespace nlohmann;
+	string filename = "file.json";
 
 	json j = {
 		{"pi", 3.141},
@@ -27,11 +34,72 @@ TestCase(json_Test)
 		}}
 	};
 
-	double pi_value = j["pi"];
 	auto happy_value = j["happy"];
 	auto list_value = j["list"];
 
-	checkEqual(pi_value, 3.141);
+	checkEqual(j["pi"], 3.141);
 	checkEqual(happy_value, true);
 	checkEqual(list_value[1], 0);
+
+	ofstream out(filename);
+	out << j.dump(4);
+	out.close();
+
+	ifstream in(filename);
+	json j2;
+	in >> j2;
+
+	checkEqual(j2["name"], "Niels");
+	checkEqual(j2["answer"]["everything"], 42);
+
+	json obj = j2["object"];
+
+	checkEqual(obj["currency"], "USD");
+	checkEqual(obj["value"], 42.99);	
+}
+
+namespace to_and_from_json_Test_namespace {
+
+	struct A
+	{
+		void set(const int x){ this->x = x; }
+		int get() const { return this->x; }
+		double z;
+		string name;
+
+	private:
+		int x;
+	};
+
+    void to_json(json& j, const A& a) {
+        j = json{{"name", a.name}, {"x", a.get()}, {"z", a.z}};
+    }
+
+    void from_json(const json& j, A& a) {
+        a.name = j.at("name").get<std::string>();
+        a.set( j.at("x").get<int>() );
+        a.z = j.at("z").get<double>();
+    }
+
+} // to_and_from_json_Test_namespace
+
+TestCase(to_and_from_json_Test)
+{
+	using namespace to_and_from_json_Test_namespace;
+
+	A a1;
+	A a2;
+	json j;
+
+	a1.name = "a1";
+	a1.z = 3.14;
+	a1.set(10);
+
+	j = a1;
+
+	a2 = j;
+
+	checkEqual(a2.name, "a1");
+	checkEqual(a2.z, 3.14);
+	checkEqual(a2.get(), 10);
 }
