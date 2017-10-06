ce008d67f922ca0a25112138971ab254a4bda345
Ruan Comelli <ruancomelli@gmail.com>
2017-10-06 18:44:48 -0300

    SimulationOutputData.py corrected (not tested)

--- Python/SimulationOutputData.py
+++ Python/SimulationOutputData.py
@@ -1,6 +1,7 @@
 import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 import matplotlib.cm as cmx
+import json
 
 class SimulationOutputData:
 
@@ -25,45 +26,57 @@ class SimulationOutputData:
 				filename = os.path.basename(boundaryFilePath)
 				boundaryName = os.path.splitext(filename)[0]
 				with open(boundaryFilePath) as boundaryFile:    
-					self.boundaryHistoryArray[particleName] = json.load(particleFile)
+					self.boundaryHistoryArray[boundaryName] = json.load(boundaryFile)
+
+			self.simulationSettings = json.load(boundaryFile)
 			
-			particleData = {}
-			boundaryData = {}
+			self.particleData = {}
+			self.boundaryData = {}
+			self.time = {}
 
 			# particleData is accessed using particleData[particleIndex][key][timeIndex][coordinate]
 			# For instance, if one wishes to access the Z coordinate, in the 5th time instant (which corresponds to 4), 
 			# of the BlueParticles's velocity, where BlueParticle is a SphericalParticle, one should write:
 			# particleData["SphericalParticle"]["BlueParticle"]["velocity"][ 4 ][ 2 ], which stands for
 			# particleData["SphericalParticle"]["BlueParticle"]["velocity"]["5th timeStep"]["Z coordinate"]
-				
+			# Similarly for boundaryData
+			# (time[i]["timeIndex"], time[i]["timeInstant"]) is the i-th timepair
+			
+			# Read simulation settings
+			simulationSettingsFilePath = paths.getSimulationMainOutputFilePath()
+			with open(simulationSettingsFilePath) as simulationSettingsFile:
+				self.simulationSettings = json.load(simulationSettingsFile)
+
+			# Read particles
 			for particleHistory in self.particleHistoryArray:
 				for particleHistoryElement in particleHistory:
-					timeIndex = particleHistoryElement["TimeIndex"]
+					timeIndex = particleHistoryElement["timeIndex"]
+					particleType = particleHistoryElement["particleType"]
+					particle = particleHistoryElement["particle"]
+					particleName = particle["Name"]
 
-					thisParticleName = "Particle" + str(counter)
-
-					thisParticleOutputFolder = simulationNumericalOutputFolder + thisParticleName + "/"
-
-					thisParticleData = {}
-
-					# It is bad. If we add anything different in our main program,
-					# we will need to change everything above. It could be interesting
-					# to put a 'listdir' and, with its return, create the plots.
-					# Plans for the future.
-
-					thisParticleData["main"] = getTagValuePairsFromFile( thisParticleOutputFolder + "data.txt", "<", ">" )
-					thisParticleData["energy"] = readCSVmatrix( thisParticleOutputFolder + "energy.txt" )
-					thisParticleData["position"] = readCSVmatrix( thisParticleOutputFolder + "position.txt" )
-					thisParticleData["velocity"] = readCSVmatrix( thisParticleOutputFolder + "velocity.txt" )
-					thisParticleData["linear_momentum"] = readCSVmatrix( thisParticleOutputFolder + "linear_momentum.txt" )
-					thisParticleData["orientation"] = readCSVmatrix( thisParticleOutputFolder + "orientation.txt" )
-					thisParticleData["rotational_velocity"] = readCSVmatrix( thisParticleOutputFolder + "rotational_velocity.txt" )
-					thisParticleData["angular_momentum"] = readCSVmatrix( thisParticleOutputFolder + "angular_momentum.txt" )
-					thisParticleData["force"] = readCSVmatrix( thisParticleOutputFolder + "force.txt" )
-					thisParticleData["torque"] = readCSVmatrix( thisParticleOutputFolder + "torque.txt" )
-
-					particleData.append(thisParticleData)
+					for key in particle.keys():
+						self.particleData[particleType][particleName][key][timeIndex] = particle[key]
 				
+			# Read boundaries
+			for boundaryHistory in self.boundaryHistoryArray:
+				for boundaryHistoryElement in boundaryHistory:
+					timeIndex = boundaryHistoryElement["timeIndex"]
+					boundaryType = boundaryHistoryElement["boundaryType"]
+					boundary = boundaryHistoryElement["boundary"]
+					boundaryName = boundary["Name"]
+
+					for key in boundary.keys():
+						self.boundaryData[boundaryType][boundaryName][key][timeIndex] = boundary[key]
+
+			# Read time
+			with open(paths.getSimulationTimeVectorPath()) as timeVectorFile:    
+				timeHistory = json.load(timeVectorFile)
+
+				for timeHistoryElement in timeHistory:
+					timeIndex = timeHistoryElement["timeIndex"]
+					timeInstant = timeHistoryElement["timeInstant"]
+					self.time[timeIndex] = timeInstant
 
 		except FileNotFoundError:
 			self.display.message("An output file wasn't found. Please, rerun simulation." )
@@ -74,7 +87,7 @@ class SimulationOutputData:
 		cmap = plt.get_cmap('gist_rainbow')
 		scalarMap = cmx.ScalarMappable( 
 			cmap = cmap,
-			norm = colors.Normalize( vmin=0 , vmax=simulationSettings["nParticles"]-1 ) )
+			norm = colors.Normalize( vmin=0 , vmax=len(particleHistoryArray) + len(boundaryHistoryArray) ) )
 
 		# Return requested data
-		return [simulationSettings , particleData , timeVectorForPlot , scalarMap]
+		return [self.simulationSettings, self.particleData, self.boundaryData, self.time, scalarMap]
