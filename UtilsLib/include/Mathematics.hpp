90748def6dad64ba3ce3c005b38a822398fbf27d
Ruan Comelli <ruancomelli@gmail.com>
2017-11-29 15:29:14 -0200

    signum (the sign of a number) defined

--- UtilsLib/include/Mathematics.hpp
+++ UtilsLib/include/Mathematics.hpp
@@ -13,6 +13,21 @@ using boost::math::constants::pi;
 //	Calculates n!, where n is a nonnegative integer
 unsigned long factorial( const unsigned n );
 
+template <typename T> inline constexpr
+int signum(T x, std::false_type is_signed) {
+    return T(0) < x;
+}
+
+template <typename T> inline constexpr
+int signum(T x, std::true_type is_signed) {
+    return (T(0) < x) - (x < T(0));
+}
+
+template <typename T> inline constexpr
+int signum(T x) {
+    return signum(x, std::is_signed<T>());
+}
+
 } // psin
 
 #endif
